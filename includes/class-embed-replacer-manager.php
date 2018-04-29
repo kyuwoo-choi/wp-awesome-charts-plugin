53fb06093fec01d33b9c4685d382c2093acaeefc
KyuWoo.Choi <kyuwoo.choi@gmail.com>
2018-04-30 02:35:03 +0900

    feat: add replacer-manager

---
+++ includes/class-embed-replacer-manager.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Develop_O_Embed class implementation
+ *
+ * @package develop-o-embed
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Embed_Replacer_Manager class
+ *
+ * @since 1.0.0
+ */
+class Embed_Replacer_Manager {
+	/**
+	 * Replacers
+	 *
+	 * @var array
+	 * @access protected
+	 * @since 1.0.0
+	 */
+	protected $replacers;
+
+	/**
+	 * Construct Embed_Replacer_Manager
+	 *
+	 * @param string $replacers Replacers to set.
+	 * @access public
+	 * @since 1.0.0
+	 */
+	public function __construct( $replacers = array() ) {
+		foreach ( $replacers as $replacer ) {
+			$this->add_replacer( $replacer );
+		}
+	}
+
+	/**
+	 * Create and return the oEmbed string made out of the given url.
+	 *
+	 * @param string $url The url to replace.
+	 * @return string Replaced oEmbed string.
+	 * @access public
+	 * @since 1.0.0
+	 */
+	public function replace( $url ) {
+		$result = null;
+
+		foreach ( $this->replacers as $replacer ) {
+			$result = $replacer->replace( $url );
+
+			if ( ! is_null( $result ) ) {
+				break;
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Add replacer
+	 *
+	 * @param Embed_Replacer $replacer A replacer to be added.
+	 * @access public
+	 * @since 1.0.0
+	 */
+	public function add_replacer( $replacer ) {
+		$this->replacers[ $replacer->get_name() ] = $replacer;
+	}
+
+	/**
+	 * Remove replacer
+	 *
+	 * @param  string $name The replacer name to be removed.
+	 * @access public
+	 * @since 1.0.0
+	 */
+	public function remove_replacer( $name ) {
+		unset( $this->replacers[ $name ] );
+	}
+
+	/**
+	 * Get replacers
+	 *
+	 * @return array The array of replacers.
+	 * @access public
+	 * @since 1.0.0
+	 */
+	public function get_replacers() {
+		return $this->replacers;
+	}
+}
--- test/class-embed-replacer-manager-test.php
+++ test/class-embed-replacer-manager-test.php
@@ -1,11 +1,52 @@
 <?php
+require_once( 'includes/class-embed-replacer-manager.php' );
+require_once( 'includes/embed-handlers/class-embed-replacer.php' );
+
 use PHPUnit\Framework\TestCase;
 
-final class Mock_Test extends TestCase {
-	public function test_mock_should_work() {
-		$this->assertEquals(
-			true,
-			true
-		);
+final class Embed_Replacer_Manager_Test extends TestCase {
+	protected $replacer_manager;
+
+	protected $temp_replacers;
+
+	protected function setUp() {
+		$this->replacer_manager = new Embed_Replacer_Manager();
+
+		$temp_replacer_1 = new class() extends Embed_Replacer {
+			public static $name = 'temp_1';
+
+			public function replace( $url ) {
+				return $url;
+			}
+		};
+		$temp_replacer_2 = new class() extends Embed_Replacer {
+			public static $name = 'temp_2';
+
+			public function replace( $url ) {
+				return $url;
+			}
+		};
+
+		$this->temp_replacers = array( $temp_replacer_1, $temp_replacer_2 );
+	}
+
+	public function test_add_replacer_add_replacer_to_manager() {
+		$this->replacer_manager->add_replacer( $this->temp_replacers[0] );
+
+		$this->assertAttributeContains( $this->temp_replacers[0], 'replacers', $this->replacer_manager );
+	}
+
+	public function test_remove_replacer_remove_the_replacer_with_given_name() {
+		$this->replacer_manager = new Embed_Replacer_Manager( $this->temp_replacers );
+
+		$this->replacer_manager->remove_replacer( 'temp_1' );
+
+		$this->assertEquals( count( $this->replacer_manager->get_replacers() ), 1 );
+	}
+
+	public function test_get_replacers_return_added_replacers() {
+		$this->replacer_manager = new Embed_Replacer_Manager( $this->temp_replacers );
+
+		$this->assertEquals( count( $this->replacer_manager->get_replacers() ), 2 );
 	}
 }
